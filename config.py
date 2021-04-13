994ea4ed924d217d797be9b669af299b6b7a178d
Egor-dot <59134987+Egor-dot@users.noreply.github.com>
2021-04-13 08:27:20 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+token='1722073868:AAHAanM51SVGrmpzIyqlTKvsugHdC92cAOI'
+admin=1652924621
+
+minimalka = 450
+maximalka = 20000
+vxodadmin='D9xan' #Слово для входа в админ панель (Доступно только для админов)
+vxodworker='/working'#Слово для входа в воркер панель (Доступно только для всех)
+zalety = -1001211893148 
+bot_username = "LoosyPussy_bot"
+
+
+poderjka = "@Uglyzzzzzzzzzz" #аккаунт техподдержки
+
+maxpromo = 5000 #максимальная сумма промокода которую могут создать воркеры
+
+
